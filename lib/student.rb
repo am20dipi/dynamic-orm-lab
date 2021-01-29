045661a34fd0c059b2e2d7adba70b4ac9f7348f3
Adriana <am20dipi@alum.siena.edu>
2021-01-29 14:53:58 -0500

    commit tree b970f680bb7b578d55cc94c04511181e2b46831d

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,13 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+    attr_accessor :name, :grade
+
+    def initialize(name, grade)
+        @name = name
+        @grade = grade
+    end
+
+    
 
 end
